c6bae2f8da514dc892c1829a2dc21091ba725aa8
Nikita Grishunin <nikitagrishn@gmail.com>
2025-04-26 20:23:14 +0500

    login + registration

--- EventMaster/urls.py
+++ EventMaster/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('accounts/', include("django.contrib.auth.urls")),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,46 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+
 
 def home(request):
-    return render(request, 'main/index.html')
+    return render(request, "main/index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return JsonResponse({"status": "success"})
+        else:
+            return JsonResponse(
+                {"status": "error"}, status=400
+            )  # возвращаем ошибку при неправильных данных
+
+    return render(request, "main/index.html")
+
+def register_view(request):
+    if request.method == 'POST':
+        login = request.POST.get('login')
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if login and name and surname and email and password:
+            try:
+                user = User.objects.create_user(username=login, email=email, password=password, first_name=name,
+                                                last_name=surname)
+                return JsonResponse({'success': True})
+            except Exception as e:
+                return JsonResponse({'success': False, 'message': str(e)})
+        else:
+            return JsonResponse({'success': False, 'message': 'Не все поля заполнены'})
+    
